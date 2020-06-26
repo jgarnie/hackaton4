77e7e3fab74cd3129fc4c594247f45f5d17c170b
jgarnie <garcia.nieto.joaquin@gmail.com>
2020-06-26 09:27:45 +0200

    first com

---
+++ src/index.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App/App.jsx";
+import "./style.scss";
+import "./index.html";
+
+ReactDOM.render(<App />, document.querySelector("#app"));
